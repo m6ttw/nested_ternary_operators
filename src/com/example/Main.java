d5dae3c3fab1412bc029d004218bf72705a6b460
MW <matthewrhyswilliams@live.com>
2021-06-17 00:01:24 +0100

    nested ternary tested

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -16,5 +16,16 @@ public class Main {
 //      standard one line ternary operator
         String newResult = newNumber % 2 == 0 ? "c" : "d" ;
         System.out.println(newResult);
+
+        int num = 5;
+//      nested ternary operator
+        String res = num % 2 == 0 ? "divides by two"
+                   : num % 3 == 0 ? "divides by three"
+                   : num % 5 == 0 ? "divides by five"
+                   : num % 7 == 0 ? "divides by seven"
+                   : "doesn't divide";
+        System.out.println(res);
+
+//      Looks noice
     }
 }
